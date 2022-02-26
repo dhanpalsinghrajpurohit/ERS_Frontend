21e63ffa5ff2ca9c9e4b2c7234425ea7ec8c9157
dhanpalsinghrajpurohit <dhanpal.singh.rajpurohit09@gmail.com>
2022-02-26 19:59:42 +0530

    checkstatus ui updated

--- src/containers/Pages/JobStatus/JobStatus.js
+++ src/containers/Pages/JobStatus/JobStatus.js
@@ -1,7 +1,50 @@
 import React, { Component } from "react";
 
 class JobStatus extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            job:null,
+            data:null,
+            username:null,
+        }
+    }
+    componentDidMount(){
+        this.get_status();
+        if(localStorage.getItem('user')){
+            this.setState({username:localStorage.getItem('user')});
+        }
+    }
+
+    get_status = async(job) => {
+        const body = {
+            user:this.state.username,
+            job:job
+        }
+        await("http://localhost:8000/jobs/get_checkstatus/",{
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify(body),
+        })
+        .then(res => res.json())
+        .then(res => {
+            if(res.status==="success"){
+                this.setState({data:res.data})
+            }
+        })
+    }
     render(){
+        if(this.state.data!==null){
+            this.state.data.map((row,index)=>{
+                return (<tr>
+                    <td>{index+1}</td>
+                    <td>{row.shortlist}</td>
+                    <td>{row.selected}</td>
+                </tr>)
+            })
+        }
         return(<div className="card bg-light mb-3 mx-auto" style={{maxWidth:50+"rem"}}>
         <div className="card-header">Applied Job</div>
         <div className="card-body">
@@ -16,13 +59,7 @@ class JobStatus extends Component{
                 </tr>
             </thead>
         <tbody>
-          <tr>
-              <td>1</td>
-              <td>Software Engineer</td>
-              <td>Selected</td>
-              <td>Pending</td>
-              <td>Pending</td>
-          </tr>
+          {this.state.data}
         </tbody> 
         </table>
         </div>
