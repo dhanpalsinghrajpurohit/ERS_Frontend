3e5e22e535a2165a799c8c5c04e2b59c41b97292
dhanpalsinghrajpurohit <dhanpal.singh.rajpurohit09@gmail.com>
2022-02-28 19:53:38 +0530

    user profile and check status working

--- src/containers/Pages/Interviews/Interview.js
+++ src/containers/Pages/Interviews/Interview.js
@@ -6,26 +6,30 @@ class Interviews extends Component{
         super(props);
         this.state = {
             onchange : null,
-            value:null,
+            value:new Date(),
         }
     }
-    onchangevlaue(event){
-
-        // this.setState({value:new Date()})
+    onChange(date){
+        this.setState({value:date})
     }
     render(){
         return(
             <React.Fragment>
                 <div className="card mx-auto text-left" style={{width:30+'%',marginTop:1+'%',borderRadius:5+'px',textAlign:"left"}}>
-                            <h5 className="card-header"></h5>
+                            <h5 className="card-header">Software Engineer</h5>
                             <div className="card-body">
-                                <h5 className="card-title">Description:Software Engineer </h5>
+                                <h6 className="card-title">Description:Software Engineer </h6>
                                 <p className="card-text">Skill: Python Django</p>
                                 <p className="card-text">Vacancy: 2</p>
                                 <p className="card-text">Salary: 100000</p>
                             </div>
-                            <div className="card-footer mx-auto">
-                            <DateTimePicker onChange={this.onchangevlaue} value={this.state.value} />
+                            <div className="card-footer mx-auto w-100">
+                                <div>
+                                    <DateTimePicker className="mx-auto" onChange={(date)=>this.onChange(date)} value={this.state.value} minDate={new Date()}/>
+                                </div>
+                                <div>
+                                    <button className="btn btn-primary m-1 mx-auto w-100">Apply</button>
+                                </div>
                             </div>                                
                 </div>
             </React.Fragment>
