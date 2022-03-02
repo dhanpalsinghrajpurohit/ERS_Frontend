b97c7712380609e97a0b3fbe8188ef27b6c1880b
dhanpalsinghrajpurohit <dhanpal.singh.rajpurohit09@gmail.com>
2022-03-02 11:53:34 +0530

    user profile ui added

--- src/containers/Pages/Profiles/Profiles.js
+++ src/containers/Pages/Profiles/Profiles.js
@@ -46,7 +46,7 @@ class Profile extends  Component{
         this.Skills = this.Skills.bind(this);
         this.ResumeLink  = this.ResumeLink.bind(this);
         this.update_profile = this.update_profile.bind(this);
-        
+        this.insert_profile = this.insert_profile.bind(this);
     }
 
     FullName(event){
@@ -87,11 +87,11 @@ class Profile extends  Component{
                 
     }
     
-    async get_profile(){
+    get_profile(){
         const body = {
             user : localStorage.getItem('user')
         }
-        await fetch("http://localhost:8000/accounts/get_user/",{
+        fetch("http://localhost:8000/accounts/get_user/",{
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -102,19 +102,24 @@ class Profile extends  Component{
         .then(res => {
             if(res.status==="success"){
                 console.log(res.data)
-                this.setState({
-                    profile:res.data,
-                    id:res.data[0].user,
-                    fullname:res.data[0].fullname,
-                    email:res.data[0].email,
-                    contactnumber:res.data[0].contactNumber,
-                    college:res.data[0].college,
-                    experience:res.data[0].experience,
-                    degree:res.data[0].degree,
-                    skills:res.data[0].skills,
-                    resumeLink:res.data[0].ResumeLink,
-                });
-                this.profileForm();
+                if(res.data.length!==0){
+                    this.setState({
+                        profile:res.data,
+                        id:res.data[0].user,
+                        fullname:res.data[0].fullname,
+                        email:res.data[0].email,
+                        contactnumber:res.data[0].contactNumber,
+                        college:res.data[0].college,
+                        experience:res.data[0].experience,
+                        degree:res.data[0].degree,
+                        skills:res.data[0].skills,
+                        resumeLink:res.data[0].ResumeLink,
+                    });
+                    this.profileForm();
+                }
+                else{
+                    this.insert_profileForm()
+                }
             }
             else{
                 this.setState({
@@ -158,6 +163,38 @@ class Profile extends  Component{
         })
     }
     
+    async insert_profile(event){
+        event.preventDefault();
+        const body = {
+            'user': localStorage.getItem('user'),
+            'fullname':this.state.fullname,
+            'email':this.state.email,
+            'contactNumber':this.state.contactnumber,
+            'college':this.state.college,
+            'experience':this.state.experience,
+            'skills':this.state.skills,
+            'degree':this.state.degree,
+            'ResumeLink':this.state.resumeLink,
+        }
+        console.log(body);
+        await fetch("http://localhost:8000/accounts/insert_user/",{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify(body)
+        })
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+            if(res.status==="success"){
+                this.setState({message:res.message})
+            }
+            else{
+                this.setState({message:res.message})
+            }
+        })
+    }
 
     profileForm = () => {
         this.setState({form:( 
@@ -229,13 +266,84 @@ class Profile extends  Component{
         )});
     }
 
+    insert_profileForm = () => {
+        console.log("insert_profileForm")
+        this.setState({form:( 
+            <div className="card mx-auto" style={{width:30+'%',marginTop:5+'%',borderRadius:5+'px'}}>
+                <div className="card-header">
+                    <h1>Profile</h1>
+                </div>
+                <div className="card-body">
+                    <form method="post" onSubmit={this.insert_profile}>
+
+                        <div className="form-group m-2">
+                            <label htmlFor="fullname">Full Name</label>
+                            <input type="text" className="form-control" id="fullname" placeholder="Enter Full Name" onChange={(e)=>this.FullName(e)} required minLength={8}  maxLength={30}/>
+                        </div>
+
+                        <div className="form-group m-2">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" className="form-control" id="email" placeholder="Enter Email" onChange={(e)=>this.Email(e)} required />
+                        </div>
+
+                        <div className="form-group m-2">
+                            <label htmlFor="contactnumber">Contact Number </label>
+                            <input type="text" className="form-control" id="contactnumber" placeholder="Enter Contact Number" onChange={(e)=>this.ContactNumber(e)} required minLength={10} maxLength={10}/>
+                        </div>
+
+                        <div className="form-group m-2">
+                        <label htmlFor="degree">Degree</label>
+                            <select className="form-select mb-3" id="degree" onChange={(e)=>this.Degree(e)} required>
+                                <option value="0">B.TECH</option>
+                                <option value="1">M.TECH</option>
+                                <option value="2">MCA</option>
+                                <option value="3">BCA</option>
+                                <option value="4">B.Sc</option>
+                                <option value="5">M.Sc</option>
+                            </select>   
+                        </div>
+                        
+                        <div className="form-group m-2">
+                            <label htmlFor="college">College</label>
+                            <input type="text" className="form-control" id="college" placeholder="Enter College" onChange={(e)=>this.College(e)} required/>
+                        </div>
+                        
+                        <div className="form-group m-2">
+                            <label htmlFor="experience">Experience</label>
+                            <select className="form-select mb-3" id="experience" onChange={(e)=>this.Experience(e)} required>
+                                <option value="0">FRESHER</option>
+                                <option value="1">1 Year</option>
+                                <option value="2">2 Year</option>
+                                <option value="3">3+ Year</option>
+                                <option value="4">5+ Year</option>
+                            </select>            
+                        </div>
+
+                        <div className="form-group m-2">
+                            <label htmlFor="skills">Skills</label>
+                            <input type="text" className="form-control" id="skills" placeholder="Enter Skills"  onChange={(e)=>this.Skills(e)} required />
+                        </div>
+
+                        <div className="form-group m-2">
+                            <label htmlFor="resumelink">Resume Link</label>
+                            <input type="text" className="form-control" id="resumelink" placeholder="Enter Resume Link"  onChange={(e)=>this.ResumeLink(e)} required />
+                        </div>
+                    
+                        <button type="submit" className="btn btn-primary">Update</button>
+                    </form>
+
+                </div>
+            </div>
+        )});
+    }
+
     render(){
         return(
             <React.Fragment>
                 <Navbar />
                 <div>
                     {this.state.message}
-                    {this.state.profile?this.state.form:null}
+                    {this.state.form?this.state.form:null}
                 </div>
                 <div>
                     <div className="card mx-auto" style={{width:80+'%',marginTop:5+'%',borderRadius:5+'px'}}>
